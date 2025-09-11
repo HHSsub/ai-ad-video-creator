d2631ab4d3c6b0ce42f447caaf972051a48a590b
HHSsub <86719426+HHSsub@users.noreply.github.com>
2025-09-11 16:20:13 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,15 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3000',
+        changeOrigin: true
+      }
+    }
   }
 })
