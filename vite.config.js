00ffcb900d65560020547ebead5145c5f0dac86d
HHSsub <86719426+HHSsub@users.noreply.github.com>
2025-09-11 16:37:07 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  },
   server: {
     host: '0.0.0.0',
     port: 5173,
@@ -18,4 +13,4 @@ export default defineConfig({
       }
     }
   }
-})
+});
