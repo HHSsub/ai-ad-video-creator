ffa0f9baf2973da2c16f85eeda27a6df147357e8
HHSsub <86719426+HHSsub@users.noreply.github.com>
2025-09-21 23:46:13 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     hmr: { host: '13.222.179.138', port: 5173, protocol: 'ws' },
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:3000',
+        target: 'http://localhost:3000',  // 백엔드 서버 포트
         changeOrigin: true,
         secure: false,
         configure: (proxy, options) => {
